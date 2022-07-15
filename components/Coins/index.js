f0c2510d632da14c1f272b8d22e3269abb483be5
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2022-07-15 16:26:16 +0200

    Final version done

---
+++ components/Coins/index.js
@@ -0,0 +1,47 @@
+import styles from "./Coins.module.css";
+import Link from 'next/link'
+
+const Coins = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+  id,
+}) => {
+  // console.log(name)
+  return (
+    <Link href='/coin/[id]' as={`/coin/${id}`}>
+    <a>
+    <div className={styles.coin_container}>
+      <div className={styles.coin_row}>
+        <div className={styles.coin}>
+          <img src={image} alt={name} className={styles.coin_img} />
+          <h1 className={styles.coin_h1}>{name}</h1>
+          <p className={styles.coin_symbol}>{symbol}</p>
+        </div>
+        <div className={styles.coin_data}>
+          <p className={styles.coin_price}>R{price}</p>
+          <p className={styles.coin_volume}>R{volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className={styles.coin_percemt, styles.red}>{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className={styles.coin_percemt, styles.green}>{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className={styles.coin_marketcap}>
+            Mkt Cap: R{marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+    </a>
+    </Link>
+    
+  );
+};
+
+export default Coins;
---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Coins from './Coins';
+
+
+const Footer = () => {
+  return (
+    <div className="footer_Container">
+        <div className="footer_Container">
+            <div>
+                <h1 className="w-32 py-[20x] font-bold text-white text-xl sm:text-3xl" >Made with ♥‿♥ by Tshembani Mhlongo aka 0xRedPill😁</h1>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Footer;
